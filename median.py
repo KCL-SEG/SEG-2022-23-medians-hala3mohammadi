9576b54c4c6e4435631092cfe67ecfa4ef38fda1
Hala Almohammadi <hala.almohammadi@kcl.ac.uk>
2023-09-30 15:56:55 +0100

    Add the median of a list

--- median.py
+++ median.py
@@ -10,3 +10,14 @@ while True:
     else:
         break
 print(numbers)
+
+numbers.sort()     
+length = len(numbers)
+median=0.0
+
+if length%2!=0:
+    median = numbers[length//2]
+else:
+    median = (numbers[length//2] + numbers[(length//2)-1]) / 2
+
+print(f"The median is {median}")
